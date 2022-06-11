a2d19948b746aafa93d0357f78c001551c89f4ef
Servet Erkan <servet.erkan@hotmail.com>
2022-06-12 00:41:47 +0300

    fix

---
+++ Infrastructure/Odeon.DataAccess/Repositories/Logs/LogWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Odeon.Application.Repositories;
+using Odeon.DataAccess.Contexts;
+using Odeon.Entities;
+
+namespace Odeon.DataAccess.Repositories
+{
+    public class LogWriteRepository : WriteRepository<Log>, ILogWriteRepository
+    {
+        public LogWriteRepository(OdeonDbContext context) : base(context) { }
+    }
+}
